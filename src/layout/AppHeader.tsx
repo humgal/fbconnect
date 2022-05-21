9133bdfa3bd94652e96903ed9ec2c145e18ca778
dl <humgal@outlook.com>
2022-05-21 12:09:18 +0800

    add appheader

---
+++ src/layout/AppHeader.tsx
@@ -0,0 +1,95 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Container from '@mui/material/Container';
+import {getCookie} from 'src/util/helpers'
+import HeaderNavBar from 'src/components/header/HeaderNavBar';
+
+import ThemeModeToggle from 'src/components/header/ThemeModeToggle';
+
+import Link from 'src/components/Link';
+
+import ROUTES from 'src/route';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+
+const Header = styled('header')(({ theme }) => ({
+  position: 'sticky',
+  top: 0,
+  transition: theme.transitions.create('top'),
+  zIndex: theme.zIndex.appBar,
+  backdropFilter: 'blur(20px)',
+  boxShadow: `inset 0px -1px 1px ${
+    theme.palette.mode === 'dark' ? theme.palette.grey[700] : theme.palette.grey[100]
+  }`,
+  backgroundColor:
+    theme.palette.mode === 'dark'
+      ? alpha(theme.palette.grey[900], 0.72)
+      : 'rgba(255,255,255,0.72)',
+}));
+
+export default function AppHeader() {
+ 
+  const [mode, setMode] = React.useState<string | null>(null);
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+
+
+  React.useEffect(() => {
+    const initialMode = getCookie('paletteMode') || 'system';
+    setMode(initialMode);
+  }, []);
+
+  const handleChangeThemeMode = (checked: boolean) => {
+    const paletteMode = checked ? 'dark' : 'light';
+    setMode(paletteMode);
+
+    document.cookie = `paletteMode=${paletteMode};path=/;max-age=31536000`;
+    
+  };
+
+  return (
+    <Header>
+      <Container sx={{ display: 'flex', alignItems: 'center', minHeight: 56 }}>
+        <Box
+          component={Link}
+          href={ROUTES.home}
+          aria-label="Go to homepage"
+          sx={{ lineHeight: 0, mr: 2 }}
+        >
+        </Box>
+        <Box sx={{ display: { xs: 'none', md: 'initial' } }}>
+          <HeaderNavBar />
+        </Box>
+        <Box sx={{ ml: 'auto' }} />
+        <Stack direction="row" spacing={1}>
+          
+          <Tooltip title={('appFrame.github')} enterDelay={300}>
+            <IconButton
+              component="a"
+              color="primary"
+              href="https://github.com/mui"
+              data-ga-event-category="header"
+              data-ga-event-action="github"
+            >
+              <GitHubIcon fontSize="small" />
+            </IconButton>
+          </Tooltip>
+          {mode !== null ? (
+            <ThemeModeToggle
+              checked={mode === 'system' ? prefersDarkMode : mode === 'dark'}
+              onChange={handleChangeThemeMode}
+            />
+          ) : null}
+        </Stack>
+        <Box sx={{ display: { md: 'none' }, ml: 1 }}>
+          
+        </Box>
+      </Container>
+    </Header>
+  );
+}
